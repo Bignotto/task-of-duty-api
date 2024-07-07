d1a7ac35c9e56cba99b2bea386adaa9a74a0ee11
Thiago Bignotto <bignotto@gmail.com>
2024-07-07 08:21:33 -0300

    :card_file_box: add docker compose file, prisma and prisma schema

---
+++ prisma/migrations/20240707111903_init_database/migration.sql
@@ -0,0 +1,117 @@
+-- CreateEnum
+CREATE TYPE "UserType" AS ENUM ('ADMIN', 'ORGANIZATION', 'USER');
+
+-- CreateEnum
+CREATE TYPE "TaskType" AS ENUM ('TASK', 'ORDER', 'GOAL');
+
+-- CreateEnum
+CREATE TYPE "RecurrenceType" AS ENUM ('DAILY', 'WEEKLY', 'EXACT', 'MONTHLY', 'ANNUALLY');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT,
+    "userType" "UserType" NOT NULL DEFAULT 'USER',
+    "partOfOrganizationId" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "organizations" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "fantasyName" TEXT NOT NULL,
+    "cnpj" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+
+    CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" BIGSERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "desciption" TEXT NOT NULL,
+    "recurrenceType" "RecurrenceType" NOT NULL,
+    "taskType" "TaskType" NOT NULL,
+    "createDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "dueDate" TIMESTAMP(3),
+    "organizationId" TEXT,
+    "creatorId" TEXT NOT NULL,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TaskList" (
+    "id" BIGSERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "organizationId" TEXT,
+    "creatorId" TEXT NOT NULL,
+
+    CONSTRAINT "TaskList_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_TaskToUser" (
+    "A" BIGINT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_TaskToTaskList" (
+    "A" BIGINT NOT NULL,
+    "B" BIGINT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organizations_ownerId_key" ON "organizations"("ownerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_TaskToUser_AB_unique" ON "_TaskToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_TaskToUser_B_index" ON "_TaskToUser"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_TaskToTaskList_AB_unique" ON "_TaskToTaskList"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_TaskToTaskList_B_index" ON "_TaskToTaskList"("B");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_partOfOrganizationId_fkey" FOREIGN KEY ("partOfOrganizationId") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "organizations" ADD CONSTRAINT "organizations_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TaskList" ADD CONSTRAINT "TaskList_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TaskList" ADD CONSTRAINT "TaskList_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TaskToUser" ADD CONSTRAINT "_TaskToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TaskToUser" ADD CONSTRAINT "_TaskToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TaskToTaskList" ADD CONSTRAINT "_TaskToTaskList_A_fkey" FOREIGN KEY ("A") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TaskToTaskList" ADD CONSTRAINT "_TaskToTaskList_B_fkey" FOREIGN KEY ("B") REFERENCES "TaskList"("id") ON DELETE CASCADE ON UPDATE CASCADE;
