d0e0cda2b07bf5667c224f52c94a9af07f5530ee
Thiago Bignotto <bignotto@gmail.com>
2024-07-14 20:08:30 -0300

    :hammer: add create new task use case, repository interface and in memory repository implementation

---
+++ src/repositories/tasks/inMemory/inMemoryTasksRepository.ts
@@ -0,0 +1,26 @@
+import { Prisma, Task } from "@prisma/client";
+import { ITasksRepository } from "../ITasksRepository";
+
+export class InMemoryTasksRepository implements ITasksRepository {
+  public items: Task[] = [];
+  async create(data: Prisma.TaskCreateInput) {
+    const tomorrow = new Date();
+    const duoDate = new Date(`${data.dueDate}`);
+
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const task: Task = {
+      title: data.title,
+      description: data.description,
+      dueDate: data.dueDate ? duoDate : tomorrow,
+      recurrenceType: data.recurrenceType,
+      taskType: data.taskType,
+      creatorId: `${data.creator.connect?.id}`,
+      organizationId: `${data.organization?.connect?.id}`,
+      id: BigInt(this.items.length),
+      createDate: new Date(),
+    };
+
+    this.items.push(task);
+    return task;
+  }
+}
