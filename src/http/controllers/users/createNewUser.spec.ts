3d4dbb0ac2b5b8845604e1d91e6f462418cb0916
Thiago Bignotto <bignotto@gmail.com>
2024-07-17 06:44:42 -0300

    ✅ add e2e tests to create user and authenticate

--- src/http/controllers/users/createNewUser.spec.ts
+++ src/http/controllers/users/createNewUser.spec.ts
@@ -1,3 +1,23 @@
-import { test } from "vitest";
+import { app } from "@/app";
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("ok", {});
+describe("E2E Create User Controller", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create a user", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "Mary Jane Watson",
+      email: "mj@dailybuggle.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
