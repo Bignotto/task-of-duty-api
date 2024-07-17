5966f19cec7dae30bb4083747854dc45d68d9d85
Thiago Bignotto <bignotto@gmail.com>
2024-07-17 07:03:27 -0300

    :hammer: add prisma repositories implementations

---
+++ src/repositories/invites/prisma/prismaInvitesRepository.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+import { IInvitesRepository } from "../IInvitesRepository";
+
+export class PrismaInvitesRepository implements IInvitesRepository {
+  async create(data: Prisma.UserInviteCreateInput) {
+    const invite = await prisma.userInvite.create({ data });
+    return invite;
+  }
+
+  async findById(id: string) {
+    const invite = await prisma.userInvite.findUnique({
+      where: {
+        id,
+      },
+    });
+    return invite;
+  }
+}
