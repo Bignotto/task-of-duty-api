94539fe506d7a001afb84d02cbb1434adb047803
Thiago Bignotto <bignotto@gmail.com>
2024-09-03 06:29:53 -0300

    :floppy_disk: save commit eslint

--- src/useCases/users/createNewUser.ts
+++ src/useCases/users/createNewUser.ts
@@ -25,7 +25,7 @@ export class CreateNewUserUseCase {
   constructor(
     private usersRepository: IUsersRepository,
     private invitesRepository: IInvitesRepository,
-  ) {}
+  ) { }
 
   async execute({
     name,
@@ -55,7 +55,7 @@ export class CreateNewUserUseCase {
       // cleanedPhone = phone.match(/(\d+)/g);
       cleanedPhone = phone.replace(/[^0-9]/g, '')
       if (cleanedPhone.length !== 11) throw new InvalidPhoneNumberError()
-    }
+    };
 
     const user = await this.usersRepository.create({
       name,
@@ -65,10 +65,10 @@ export class CreateNewUserUseCase {
       userType: inviteId ? UserType.USER : UserType.ORGANIZATION,
       partOfOrganization: invite
         ? {
-            connect: {
-              id: invite?.organizationId,
-            },
-          }
+          connect: {
+            id: invite?.organizationId,
+          },
+        }
         : undefined,
     })
     return { user }
