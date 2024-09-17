c704ddcb7b5ba902c7a872a3436da0da4b2d3b7b
Thiago Bignotto <bignotto@gmail.com>
2024-09-17 06:31:30 -0300

    :floppy_disk: save commit

---
+++ src/http/controllers/tasks/createTask.spec.ts
@@ -0,0 +1,37 @@
+import { app } from '@/app'
+import { createAndAssignOrganization } from '@/utils/tests/createAndAssignOrganization'
+import { createAuthenticatedUser } from '@/utils/tests/createAuthenticatedUser'
+import { addDays } from 'date-fns'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('E2E Create task Controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create new task', async () => {
+    const { token } = await createAuthenticatedUser(app)
+    await createAndAssignOrganization(app, token, {})
+
+    // TODO: fix database to handle tasks without organizations
+    // TODO: fix create task use case to handle tasks without organizations
+
+    const response = await request(app.server)
+      .post('/tasks')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Task 1',
+        description: 'first ever task',
+        recurrenceType: 'MONTHLY',
+        taskType: 'TASK',
+        dueDate: addDays(new Date(), 2),
+      })
+
+    expect(response.status).toEqual(201)
+  })
+})
